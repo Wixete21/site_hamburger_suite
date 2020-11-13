fa9526717222b0029265496ed2d42a8f799a04ee
Rémi <therrienremi@gmail.com>
2020-11-13 11:56:08 -0500

    site terminé

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //Un commentaire
     console.log("Ça fonctionne!!!");
+
+
     var informations = [
         {
             id: "pizza",
@@ -29,7 +31,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
             description: "Cake sweet tiramisu lollipop donut sprinkles dolor tiramisu marzipan jelly. Tiramisu muffin cookie orange dolor marzipan croissant sweet ipsum muffin. Tiramisu donut marzipan croissant lollipop orange cookie sugar vanilla cake. Sugar orange muffin sweet cake croissant sprinkles dolor jelly cake",    
             gallery: ["images/donuts/1.jpg", "images/donuts/2.jpg", "images/donuts/3.jpg"]
         }
-    ]
+    ];
 
 
+    if(document.querySelector('.page-item')){
+        reinitialiserItem();
+    }
+    function reinitialiserItem() {
+        var url = window.location.search;
+        console.log(url);
+
+        var params = new URLSearchParams(url);
+        console.log(params);
+
+        //Quel type de contenu on veut
+        var type =  params.get('type');
+        console.log(type);
+
+        var contenu = informations.find(element => element.id === type);
+        console.log(contenu);
+
+        var title = document.querySelector('.main-title');
+        title.innerHTML = contenu.title;
+
+        var slogan = document.querySelector('.sub-title');
+        slogan.innerHTML = contenu.slogan;
+
+        var desc = document.querySelector('.sub-description p');
+        desc.innerHTML = contenu.description;
+
+        var image = document.querySelector('.sub-image img');
+        image.src = contenu.image;
+
+        var list = document.querySelector('.type-list');
+        list.innerHTML = "";
+
+        for(let x = 0; x < contenu.type.length; x++){
+            console.log(contenu.type[x]);
+            let li = document.createElement("li");
+            li.innerHTML = contenu.type[x];
+
+            list.appendChild(li);
+        }
+
+        var gallery = document.querySelectorAll('.thumb img');
+        for(let i = 0; i < gallery.length; i++){
+            var thumb = gallery[i];
+            thumb.src = contenu.gallery[i];
+        }
+    }
 });
